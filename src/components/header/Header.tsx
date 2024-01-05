60c02e62ab7eb45aea4293259a0c5b122a3a9b66
Akbar Sharofiddinov <akbarsharofiddinov@gmail.com>
2024-01-05 17:59:28 +0500

    Change Reviews showing and fix a bug at Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -25,141 +25,164 @@ import { setCategories } from "../../store/categories/categorieSlice";
 // ====================================================================
 
 const Header = () => {
-  const [categorieState, setCategorieState] = useState<ICategories[]>([]);
-  const [showCategories, setShowCategories] = useState(false);
-  const [showMenuItems, setShowMenuItems] = useState(false);
-  const { mode, setMode } = useMainContext()!;
+    const [categorieState, setCategorieState] = useState<ICategories[]>([]);
+    const [showCategories, setShowCategories] = useState(false);
+    const [showMenuItems, setShowMenuItems] = useState(false);
+    const { mode, setMode } = useMainContext()!;
 
-  // ===============================================================
+    // ===============================================================
 
-  // Store - hooks
-  const dispatch = useAppDispatch();
-  const { categories } = useAppSelector((state) => state.Categories);
+    // Store - hooks
+    const dispatch = useAppDispatch();
+    const { categories } = useAppSelector((state) => state.Categories);
 
-  window.addEventListener("click", () => {
-    setShowCategories(false);
-    setShowMenuItems(false);
-  });
+    // Window Click event to close some Menus
+    window.addEventListener("click", () => {
+        setShowCategories(false);
+        setShowMenuItems(false);
+    });
 
-  // Navigating
-  const landingNavigate = (section_id: string) => {
-    setShowMenuItems(false);
-    setTimeout(() => {
-      scrollTo({
-        top: document.getElementById(section_id)?.offsetTop,
-      });
-    }, 200);
-  };
+    // Navigating
+    const landingNavigate = (section_id: string) => {
+        setShowMenuItems(false);
+        setTimeout(() => {
+            scrollTo({
+                top: document.getElementById(section_id)?.offsetTop,
+            });
+        }, 200);
+    };
 
-  // Theme change button
-  const handleModebutton = (checked: boolean) => {
-    localStorage.setItem("mode", JSON.stringify(checked));
-    setMode(checked);
-  };
+    // Theme change button
+    const handleModebutton = (checked: boolean) => {
+        localStorage.setItem("mode", JSON.stringify(checked));
+        setMode(checked);
+    };
 
-  async function getCategories() {
-    try {
-      const response = await axios.get("categories");
-      const data = await response.data;
+    async function getCategories() {
+        try {
+            const response = await axios.get("categories");
+            const data = await response.data;
 
-      if (response.status === 200) {
-        dispatch(setCategories(data.data));
-        setCategorieState(data.data);
-      }
-    } catch (error) {
-      console.log(error);
+            if (response.status === 200) {
+                dispatch(setCategories(data.data));
+                setCategorieState(data.data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
 
-  useEffect(() => {
-    getCategories();
-    setCategorieState(categories);
-  }, []);
+    useEffect(() => {
+        getCategories();
+        setCategorieState(categories);
+    }, []);
 
-  return (
-    <>
-      <header className={mode ? "header dark" : "header"}>
-        <Modal />
-        <div className="container">
-          <div className="header-inner">
-            <a href="/" className="logo">
-              <img src={LogoImg} alt="Logo" />
-            </a>
-            <div>
-              <ul
-                className={showMenuItems ? "header-menu show" : "header-menu"}
-                onClick={(event) => event.stopPropagation()}
-              >
-                {showMenuItems ? (
-                  <h1
-                    className="menu-title"
-                    onClick={() => setShowMenuItems(false)}
-                  >
-                    Close
-                  </h1>
-                ) : (
-                  ""
-                )}
-                <button
-                  className="header-menu_item"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setShowCategories(!showCategories);
-                  }}
-                >
-                  Каталог
-                  <IoIosArrowDown />
-                  <ul
-                    className={
-                      showCategories && showMenuItems
-                        ? "categories active"
-                        : "categories"
-                    }
-                  >
-                    {categorieState.map((category, index) => (
-                      <Link
-                        to={`/shopping/${category.slug}`}
-                        key={index}
-                        className="category"
-                        onClick={() => setShowMenuItems(false)}
-                      >
-                        {category.name}
-                      </Link>
-                    ))}
-                  </ul>
-                </button>
-                <li className="header-menu_item">
-                  <NavLink to="/" onClick={() => landingNavigate("clients")}>
-                    Довольные клиенты
-                  </NavLink>
-                </li>
-                <li className="header-menu_item">
-                  <NavLink to="/" onClick={() => landingNavigate("rate")}>
-                    Оцените нас
-                  </NavLink>
-                </li>
-                <li className="header-menu_item">
-                  <NavLink to="/" onClick={() => landingNavigate("about")}>
-                    О нас{" "}
-                  </NavLink>
-                </li>
-              </ul>
-              <Switch checked={mode} onChange={handleModebutton} />
-              <button
-                className="bar-menu"
-                onClick={(event) => {
-                  event.stopPropagation();
-                  setShowMenuItems(!showMenuItems);
-                }}
-              >
-                <AiOutlineMenu />
-              </button>
-            </div>
-          </div>
-        </div>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <header className={mode ? "header dark" : "header"}>
+                <Modal />
+                <div className="container">
+                    <div className="header-inner">
+                        <a href="/" className="logo">
+                            <img src={LogoImg} alt="Logo" />
+                        </a>
+                        <div>
+                            <ul
+                                className={
+                                    showMenuItems
+                                        ? "header-menu show"
+                                        : "header-menu"
+                                }
+                                onClick={(event) => event.stopPropagation()}
+                            >
+                                {showMenuItems ? (
+                                    <h1
+                                        className="menu-title"
+                                        onClick={() => setShowMenuItems(false)}
+                                    >
+                                        Close
+                                    </h1>
+                                ) : (
+                                    ""
+                                )}
+                                <button
+                                    className="header-menu_item"
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        setShowCategories(!showCategories);
+                                    }}
+                                >
+                                    Каталог
+                                    <IoIosArrowDown />
+                                    <ul
+                                        className={
+                                            showCategories || showMenuItems
+                                                ? "categories active"
+                                                : "categories"
+                                        }
+                                    >
+                                        {categorieState.map(
+                                            (category, index) => (
+                                                <Link
+                                                    to={`/shopping/${category.slug}`}
+                                                    key={index}
+                                                    className="category"
+                                                    onClick={() =>
+                                                        setShowMenuItems(false)
+                                                    }
+                                                >
+                                                    {category.name}
+                                                </Link>
+                                            )
+                                        )}
+                                    </ul>
+                                </button>
+                                <li className="header-menu_item">
+                                    <NavLink
+                                        to="/"
+                                        onClick={() =>
+                                            landingNavigate("clients")
+                                        }
+                                    >
+                                        Довольные клиенты
+                                    </NavLink>
+                                </li>
+                                <li className="header-menu_item">
+                                    <NavLink
+                                        to="/"
+                                        onClick={() => landingNavigate("rate")}
+                                    >
+                                        Оцените нас
+                                    </NavLink>
+                                </li>
+                                <li className="header-menu_item">
+                                    <NavLink
+                                        to="/"
+                                        onClick={() => landingNavigate("about")}
+                                    >
+                                        О нас{" "}
+                                    </NavLink>
+                                </li>
+                            </ul>
+                            <Switch
+                                checked={mode}
+                                onChange={handleModebutton}
+                            />
+                            <button
+                                className="bar-menu"
+                                onClick={(event) => {
+                                    event.stopPropagation();
+                                    setShowMenuItems(!showMenuItems);
+                                }}
+                            >
+                                <AiOutlineMenu />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default Header;
--- src/components/sections/reviews/Reviews.tsx
+++ src/components/sections/reviews/Reviews.tsx
@@ -2,9 +2,12 @@ import myAxios from "@/api";
 import { client1 } from "@/assets";
 import { useAppDispatch, useAppSelector } from "@/store/hooks/hooks";
 import { setReviews } from "@/store/reviews/reviewSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Reviews = () => {
+  // Reviews larni 10 tadan chiqarish
+  const [reviewLength, setReviewLength] = useState(4);
+
   const { reviews } = useAppSelector((state) => state.Reviews);
   const dispatch = useAppDispatch();
 
@@ -12,6 +15,8 @@ const Reviews = () => {
     try {
       const response = await myAxios.get("reviews");
       const data = await response.data;
+
+      console.log(data);
       if (response.status === 200) {
         dispatch(setReviews(data.data));
       }
@@ -24,33 +29,89 @@ const Reviews = () => {
     getReviews();
   }, []);
 
+  console.log(reviews);
+
   return (
     <>
       <section className="section-usrPleased" id="clients">
         <div className="container">
           <h1 className="title">Наши довольные клиенты</h1>
           <div className="usersFeedback">
-            {reviews.map((review, index) => (
-              <div className="userFeedback" key={`${review.full_name}_${index}`}>
-                <div className="inner">
-                  <div className="user">
-                    <img src={client1} alt="client-avatar" className="avatar" />
-                    <div>
-                      <h1>{review.full_name}</h1>
-                      <p>{review.address}</p>
+            {reviews.length > reviewLength ? (
+              <>
+                {reviews.map((review, index) => {
+                  if (index <= reviewLength - 1) {
+                    return (
+                      <div
+                        className="userFeedback"
+                        key={`${review.full_name}_${index}`}
+                      >
+                        <div className="inner">
+                          <div className="user">
+                            <img
+                              src={client1}
+                              alt="client-avatar"
+                              className="avatar"
+                            />
+                            <div>
+                              <h1>{review.full_name}</h1>
+                              <p>{review.address}</p>
+                            </div>
+                          </div>
+                          <p className="desc">{review.comment}</p>
+                        </div>
+                        <img
+                          src={review.img_url}
+                          alt="feedback-product1"
+                          className="product"
+                        />
+                      </div>
+                    );
+                  }
+                })}
+                <button
+                  className="showMoreButton"
+                  onClick={() => setReviewLength(reviews.length)}
+                >
+                  Show more
+                </button>
+              </>
+            ) : (
+              <>
+                {reviews.map((review, index) => (
+                  <div
+                    className="userFeedback"
+                    key={`${review.full_name}_${index}`}
+                  >
+                    <div className="inner">
+                      <div className="user">
+                        <img
+                          src={client1}
+                          alt="client-avatar"
+                          className="avatar"
+                        />
+                        <div>
+                          <h1>{review.full_name}</h1>
+                          <p>{review.address}</p>
+                        </div>
+                      </div>
+                      <p className="desc">{review.comment}</p>
                     </div>
+                    <img
+                      src={review.img_url}
+                      alt="feedback-product1"
+                      className="product"
+                    />
                   </div>
-                  <p className="desc">
-                    {review.comment}
-                  </p>
-                </div>
-                <img
-                  src={review.img_url}
-                  alt="feedback-product1"
-                  className="product"
-                />
-              </div>
-            ))}
+                ))}
+                <button
+                  className="collapseButton"
+                  onClick={() => setReviewLength(4)}
+                >
+                  Collapse
+                </button>
+              </>
+            )}
           </div>
         </div>
       </section>
